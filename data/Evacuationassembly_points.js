16849b72e67fc686a93f78569ebe071961800076
sabinnepal599 <163380551+sabinnepal599@users.noreply.github.com>
2024-05-06 12:26:09 +0545

    Add files via upload

---
+++ data/Evacuationassembly_points.js
@@ -0,0 +1,15 @@
+var Evacuationassembly_points = {
+"type": "FeatureCollection",
+"name": "Evacuationassembly_points",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0, "Name": "Yubra Evacuation site", "Type": "Evacuation Point", "Nep_Name": "o'a|f c:yfO{ e]nf :yn", "ORIG_FID": 0, "Area_sq_m": 637.0, "Capacity": "141" }, "geometry": { "type": "Point", "coordinates": [ 85.29660279381882, 28.006198630894382 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Name": "Kafthali Evacuation site", "Type": "Evacuation Point", "Nep_Name": "sˆynL c:yfO{ e]nf :yn", "ORIG_FID": 1, "Area_sq_m": 278.0, "Capacity": "61" }, "geometry": { "type": "Point", "coordinates": [ 85.259493868945626, 27.999106655280862 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Name": "Bhumethan Evacuation site", "Type": "Evacuation Point", "Nep_Name": "e'd]yfg c:yfO{ e]nf :yn", "ORIG_FID": 2, "Area_sq_m": 96.0, "Capacity": "21" }, "geometry": { "type": "Point", "coordinates": [ 85.264788351821579, 27.97244361752055 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Name": "Langbu Evacuation site", "Type": "Evacuation Point", "Nep_Name": "nf+j' c:yfO{ e]nf :yn", "ORIG_FID": 3, "Area_sq_m": 640.0, "Capacity": "142" }, "geometry": { "type": "Point", "coordinates": [ 85.279971896263262, 28.026068778270151 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Name": "Chhap Danda Evacuation site", "Type": "Evacuation Point", "Nep_Name": "5fk 8fF8f c:yfO{ e]nf :yn", "ORIG_FID": 4, "Area_sq_m": 184.0, "Capacity": "40" }, "geometry": { "type": "Point", "coordinates": [ 85.275590796261071, 28.010633660265825 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Name": "Ghormu Evacuation site", "Type": "Evacuation Point", "Nep_Name": "3f]d'{ c:yfO{ e]nf :yn", "ORIG_FID": 5, "Area_sq_m": 311.0, "Capacity": "69" }, "geometry": { "type": "Point", "coordinates": [ 85.254525267771882, 28.010331668645943 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Name": "Ekpakhe Evacuation site", "Type": "Evacuation Point", "Nep_Name": "PskfFv] c:yfO{ e]nf :yn", "ORIG_FID": 6, "Area_sq_m": 205.0, "Capacity": "45" }, "geometry": { "type": "Point", "coordinates": [ 85.263362183354786, 28.002639166821918 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "Name": "Badahare Evacuation site", "Type": "Evacuation Point", "Nep_Name": "a8x/] c:yfO{ e]nf :yn", "ORIG_FID": 7, "Area_sq_m": 2884.0, "Capacity": "640" }, "geometry": { "type": "Point", "coordinates": [ 85.241367637879222, 27.985297243781829 ] } }
+]
+}
